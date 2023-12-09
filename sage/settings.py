5b7ca0d03c923fc1644b84b32056e50ab1a5b114
Heet <heet149@gmail.com>
2023-12-09 00:00:47 -0500

    Debug False

--- sage/settings.py
+++ sage/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@kk$3^g#es4pcou=n!!^$6_2121q_*!ipj!jv95i@8za_*xk@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
